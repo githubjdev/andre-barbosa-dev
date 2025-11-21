6fff509174d943b22b905632376890dea854460a
afgale <alex.fernando.egidio@gmail.com>
2025-11-21 20:38:32 -0300

    .

--- andre-barbosa-dev/src/test/java/andre/dev/controller/ProdutoTest.java
+++ andre-barbosa-dev/src/test/java/andre/dev/controller/ProdutoTest.java
@@ -1,6 +1,11 @@
 package andre.dev.controller;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import andre.dev.app.SpringBootApp;
@@ -63,5 +73,103 @@ public class ProdutoTest extends TesteGeneric {
 		produtoRepository.deleteAll();
 		
 	}
+	
+	@Test
+	@DisplayName("2 - GET /api/produtos - Deve retornar lista de produtos")
+	public void testeListarTodos2() throws Exception  {
+		
+		produtoRepository.deleteAll();
+		
+		Produto p1 = new Produto("Produto A");
+		Produto p2 = new Produto("Produto B");
+		
+		p1 = produtoService.salvar(p1);
+		p2 = produtoService.salvar(p2);
+		
+		getMockMvc().perform(get("/api/produtos"))
+		            .andExpect(status().isOk())
+		            .andExpect(jsonPath("$.length()").value(2))
+		            .andExpect(jsonPath("$[0].id").value(p1.getId()))
+		            .andExpect(jsonPath("$[0].nome").value(p1.getNome()))
+		            .andExpect(jsonPath("$[1].id").value(p2.getId()))
+		            .andExpect(jsonPath("$[1].nome").value(p2.getNome()));
+		
+	}
+	
+	
+	@Test
+	@DisplayName("3 - GET /api/produtos - Deve retornar lista vazia")
+	void testeListarTodos3() throws Exception {
+		produtoRepository.deleteAll();
+		
+		ResponseEntity<Produto[]> response = restTemplate
+		                         .getForEntity(url("api/produtos"), 
+		                          Produto[].class);
+		
+		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode().value());
+		assertEquals(0, response.getBody().length);
+		
+	}
+	
+	
+	@Test
+	@DisplayName("4 - POST /api/produtos - Deve criar um novo produto")
+	void testeCriarProduto() throws Exception {
+
+		Produto produto = new Produto();
+		produto.setNome("Teclado mecânico");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		HttpEntity<Produto> resquest = new HttpEntity<Produto>(produto, headers);
+
+		ResponseEntity<Produto> response= restTemplate.postForEntity(url("api/produtos"), 
+				                          resquest, 
+				                          Produto.class);
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+		assertNotNull(response.getBody().getId());
+		assertTrue(response.getBody().getId() > 0);
+		assertNotNull(response.getBody().getNome());
+		assertEquals("Teclado mecânico", response.getBody().getNome());
+		
+	}
+	
+	
+	@Test
+	@DisplayName("5 - PUT /api/produtos - Deve ATUALIZAR produto")
+	void testeAtualizarProduto() throws Exception {
+
+		Produto produto = new Produto();
+		produto.setNome("Teclado mecânico");
+		
+		produto = produtoService.salvar(produto);
+		
+		 /*Troca o nome para testar o atualizar*/
+		produto.setNome("Teclado digital");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		HttpEntity<Produto> resquest = new HttpEntity<Produto>(produto, headers);
+
+		ResponseEntity<Produto> response= restTemplate.exchange(url("api/produtos"), 
+				                          HttpMethod.PUT,
+				                          resquest, 
+				                          Produto.class);
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+		assertNotNull(response.getBody().getId());
+		assertTrue(response.getBody().getId() > 0);
+		assertNotNull(response.getBody().getNome());
+		assertEquals("Teclado digital", response.getBody().getNome());
+		
+	}
+	
+	
+	
 
 }
